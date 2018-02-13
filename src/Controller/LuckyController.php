60110b4116e6545c93f9fa01e11ca3ef13efec9f
axelAmbonville <axel.ambonville@outlook.fr>
2018-02-13 11:56:21 +0100

    test de premier envoi

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// src/Controller/LuckyController.php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class LuckyController extends Controller
+{
+    /**
+    * @Route("/lucky/number", name="app_lucky_number")
+    */
+    public function number()
+    {
+        $number = mt_rand(0, 10);
+        if ($number==2) {
+            $number="celui-ci je le cache :) ";
+        }
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
